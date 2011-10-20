c75ee3cd020e397fc703461b12915f41a91a17c4
Stefan Haun <stefan.haun@ovgu.de>
2011-10-20 15:03:32 +0000

    * log4j-config  * configuration in transport factory
    git-svn-id: https://projects.dke-research.de/svn/tools/glue/glue-xmpp/trunk@21 75a1b36b-258a-4167-9528-f3dfdc565bb7

--- src/main/java/de/ovgu/dke/glue/xmpp/XMPPTransportFactory.java
+++ src/main/java/de/ovgu/dke/glue/xmpp/XMPPTransportFactory.java
@@ -5,19 +5,34 @@ import java.net.URI;
 import de.ovgu.dke.glue.api.transport.Transport;
 import de.ovgu.dke.glue.api.transport.TransportException;
 import de.ovgu.dke.glue.api.transport.TransportFactory;
+import de.ovgu.dke.glue.xmpp.config.XMPPConfiguration;
 import de.ovgu.dke.glue.xmpp.config.XMPPConfigurationLoader;
 
 public class XMPPTransportFactory implements TransportFactory {
 	private final XMPPConfigurationLoader configLoader;
 
+	private XMPPConfiguration config;
+
 	public XMPPTransportFactory(final XMPPConfigurationLoader configLoader) {
 		this.configLoader = configLoader;
+		this.config = null;
+	}
+
+	protected void init() throws Exception {
+		config = configLoader.loadConfiguration();
 	}
 
 	@Override
 	public Transport createTransport(URI peer) throws TransportException {
+		try {
+		if (this.config == null)
+				init();
+		
 		// TODO Auto-generated method stub
 		return null;
+		} catch (Exception e) {
+			throw new TransportException(e);
+		}
 	}
 
 }
