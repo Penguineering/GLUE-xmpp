07dbe30d4d876c87bfc9f0fe50a8f2beff66ce02
Stefan Haun <stefan.haun@ovgu.de>
2012-02-21 15:38:57 +0000

    * Licensing  * Removed Dependency to log4j
    git-svn-id: https://projects.dke-research.de/svn/tools/glue/glue-xmpp/trunk@130 75a1b36b-258a-4167-9528-f3dfdc565bb7

--- src/main/java/de/ovgu/dke/glue/xmpp/serialization/CapabilitiesSerializationProviderWrapper.java
+++ src/main/java/de/ovgu/dke/glue/xmpp/serialization/CapabilitiesSerializationProviderWrapper.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2012 Stefan Haun, Thomas Low, Sebastian Stober, Andreas Nürnberger
+ * 
+ *      Data and Knowledge Engineering Group, 
+ * 		Faculty of Computer Science,
+ *		Otto-von-Guericke University,
+ *		Magdeburg, Germany
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.ovgu.dke.glue.xmpp.serialization;
 
 import java.util.List;
