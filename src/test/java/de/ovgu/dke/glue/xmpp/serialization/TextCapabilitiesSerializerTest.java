006b764b8d58eb808c16e0facebaecf6858ec1d0
Sebastian Dorok <sebastian.dorok@st.ovgu.de>
2012-07-27 11:10:34 +0000

    [test]
    git-svn-id: https://projects.dke-research.de/svn/tools/glue/glue-xmpp/trunk@210 75a1b36b-258a-4167-9528-f3dfdc565bb7

---
+++ src/test/java/de/ovgu/dke/glue/xmpp/serialization/TextCapabilitiesSerializerTest.java
@@ -0,0 +1,11 @@
+package de.ovgu.dke.glue.xmpp.serialization;
+
+import de.ovgu.dke.glue.test.serialization.AbstractSerializerTests;
+
+public class TextCapabilitiesSerializerTest extends AbstractSerializerTests {
+
+	public TextCapabilitiesSerializerTest() {
+		super(new TextCapabilitiesSerializer());
+	}
+
+}
